33fd64a1f7813a9df12bb0ce682663f5f13c325a
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-10-30 10:03:23 +0100

    Algo1 done

--- src/Algorithms2.java
+++ src/Algorithms2.java
@@ -7,7 +7,7 @@
 
 public class Algorithms2 {
 
-    // TODO 1 Skriv en metode her i denne klasse, som kan finde et tal i Data klassens randomNumbers1000 felt med LINEAR SEARCH
+    // * TODO 1 Skriv en metode her i denne klasse, som kan finde et tal i Data klassens randomNumbers1000 felt med LINEAR SEARCH
 
     // TODO 2 Skriv en metode her i denne klasse, som kan finde et navn i Data klassens randomDrengeNavne felt med LINEAR SEARCH
 
@@ -20,4 +20,63 @@ public class Algorithms2 {
     // TODO 6 Skriv en metode her i denne klasse, som kan finde et navn i Data klassens randomDrengeNavne felt med BINARY SEARCH
 
 
-}
+    public static void main(String[] args) {
+        System.out.println("*************lineSearch of array for chosen num");
+        Algorithms2 a2 = new Algorithms2();
+        System.out.println(a2.lineSearch(271));
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+    public String lineSearch(int n){
+        Data data = new Data();
+
+        int[] intArray = data.getRandomNumbers1000();
+
+        for (int i = 0; i < intArray.length; i++) {
+            if (n == intArray[i])
+                return "The number is " + n + " and is located in the array on the " + i + " position:";
+            }
+            return "The number " + n + " is not in the array";
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
